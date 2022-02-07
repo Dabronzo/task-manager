e00b0c85d3606182c142dbbd6ffc650d25928836
Dabronzo <ayrtonterceiro@gmail.com>
2022-02-07 19:52:47 +0000

    Added the task creator and display

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
-  });
+    M.Sidenav.init(sidenav);
+
+    //date pickers
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd, mmmm, yyyy",
+      i18: {done: "Select"}
+    });
+
+    //dropdown select
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
+
+    // toggle tasks description
+    var collapse = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapse);
+
+});
